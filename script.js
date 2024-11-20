e7acf2b0528e88b88c92347aba98618e321a0df9
iadityasingh-3616 <iadityasingh2468@gmail.com>
2024-11-20 23:23:08 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,32 @@
+        const heart = document.getElementById('heart');
+        const postImg = document.querySelector('.post-img');
+
+        postImg.addEventListener('dblclick', () =>{
+
+            heart.classList.replace('far','fas');
+            heart.style.color ='red';
+            document.querySelector('.react-detail').innerHTML ='2,407 likes';
+            postImg.classList.add('active');
+
+            setInterval(() => {
+                postImg.classList.remove('active');
+            }, 2000);
+        });
+
+        heart.addEventListener('click',handleHeart)
+
+        function handleHeart() {
+            var attr = heart.getAttributeNode("class").value;
+
+            if(attr == 'far fa-heart'){
+                heart.classList.replace('far','fas');
+                heart.style.color = 'red';
+                document.querySelector('.react-detail').innerHTML = '2,407 likes';
+            }
+            else if(attr == 'fas fa-heart'){
+                heart.classList.replace('fas','far');
+                heart.style.color = 'white';
+                document.querySelector('.react-detail').innerHTML = '2,406 likes';
+            }
+        }
+    
